01b16b4b122f82700011e1b99cad16592ac8b136
Dhruvi Desai <dhruvidesai1844@gmail.com>
2022-04-24 23:27:10 -0700

    about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,12 +7,13 @@ const url =
   request({ url, json: true }, (error, { body }) => {
         const temp = body.current.temperature;
         const feels = body.current.feelslike;
+        const humidity = body.current.humidity;
         if(error) {
             callback('Unable to connect weatherstack', undefined)
         } else if (body.error) {
             callback('Unable to find location.');
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". current temparature is " + temp + ". it feels like to reach at " + feels)
+            callback(undefined, body.current.weather_descriptions[0] + ". current temparature is " + temp + ". it feels like to reach at " + feels + ". Humidity is also " + humidity + '%.');
         }
   })
 
